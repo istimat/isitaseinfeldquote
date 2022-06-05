cd080ab30d983fafd1871ef8a6d4d4e8a3503826
istimat <istimat@gmail.com>
2022-06-05 17:42:01 +0300

    working on parsing csv

--- read_csv.js
+++ read_csv.js
@@ -2,8 +2,11 @@ const fs = require('fs');
 
 const allFileContents = fs.readFileSync('scripts.csv', 'utf-8');
 allFileContents.split(/\r?\n/).forEach(line =>  {
-  
+    const splitLine = line.split(",")
+    const season = splitLine[0]
+    const episode = splitLine[1]
+    const actor = splitLine[2]
+    const dialogue = splitLine[3]
     
-  
-    console.log(`Line from file: ${line}`);
+    console.log(`Line from file: ${dialogue}`);
 });
