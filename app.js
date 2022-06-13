a7666740d23c3da6388561e5a35c94dcd3a26f97
Istvan Matuz <istimat@gmail.com>
2022-06-13 20:09:20 +0300

    read db succesfully

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
-var msg = "Hello World"
-console.log(msg)
+const fs = require('fs');
+
+const DialogueRepository = require('./dialogueRepository');
+const AppDAO = require('./dao');
+
+
+
+function main() {
+    const dao = new AppDAO('./database.sqlite3')
+    const dialogueRepo = new DialogueRepository(dao)
+
+
+
+     dialogueRepo.getAll()
+     .then((rows) => {
+        rows.forEach((row) => console.log(row));
+        })
+    
+}
+
+main()
