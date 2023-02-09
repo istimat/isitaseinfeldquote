dc1caad8a08cbd166d544b7aca4d37b403b0bd6f
Istvan Matuz <istimat@gmail.com>
2023-02-09 20:27:06 +0200

    removed unused index router

--- app.js
+++ app.js
@@ -10,11 +10,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
 const bodyParser = require('body-parser')
 var app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -24,7 +22,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-//app.use('/', indexRouter);
 
 
 app.get("/", (req, res) => {
